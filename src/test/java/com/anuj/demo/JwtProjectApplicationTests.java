3f03f3d702cf9f9e048c1b7a83f691791f49b022
Anuj Parmar <anuj.parmar@fisglobal.com>
2020-09-01 00:15:14 +0530

    first commit

---
+++ src/test/java/com/anuj/demo/JwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
